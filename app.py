54b53dbad88d97b6260b10a83100c1715b4db0f7
Jivraj-18 <22f3002542@ds.study.iitm.ac.in>
2025-07-24 16:27:57 +0000

    session 1

--- app.py
+++ app.py
@@ -4,13 +4,15 @@
 #   "fastapi",
 #   "python-multipart",
 #   "uvicorn",
+#   "google-genai",
 # ]
 # ///
 
 from fastapi import FastAPI,File, UploadFile    
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-
+from google import genai
+import os
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"]) # Allow GET requests from all origins
@@ -22,7 +24,23 @@ app.add_middleware(
     allow_methods=["*"],  # Allow specific methods
     allow_headers=["*"],  # Allow all headers
 )
+def task_breakdown(task:str):
+    """Breaks down a task into smaller programmable steps using Google GenAI."""
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+    task_breakdown_file = os.path.join('prompts', "abdul_task_breakdown.txt")
+    with open(task_breakdown_file, 'r') as f:
+        task_breakdown_prompt = f.read()
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-lite",
+        contents=[task,task_breakdown_prompt],
+    )
+    
+    with open("abdul_breaked_task.txt", "w") as f:
+        f.write(response.text)
+
+    return response.text
 
 @app.get("/")
 async def root():
@@ -34,6 +52,8 @@ async def upload_file(file: UploadFile = File(...)):
     try:
         content = await file.read()
         text = content.decode("utf-8")  # assuming it's a text file
+        breakdown = task_breakdown(text)
+        print(breakdown)
         return {"filename": file.filename, "content": text}
     except Exception as e:
         return JSONResponse(status_code=400, content={"error": str(e)})
